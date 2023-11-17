3234bdcc2394190fc0414c5efecf6e9cd422b666
Michael Nascimento <michael.nsc@outlook.com>
2023-11-17 14:58:53 -0300

    finaliza login page

--- components/pages/HomePage.tsx
+++ components/pages/HomePage.tsx
@@ -1,11 +1,23 @@
+import imageBg from "../../assets/images/image-bg.avif";
 import { AccountBox } from "../AccountBox";
 
 export const HomePage = () => {
   return (
-    <div className="container mx-auto h-100">
-      <div className="row h-100 align-items-center justify-content-end">
-        <div className="col-4">
-          <AccountBox />
+    <div
+      className="h-100"
+      style={{
+        // backgroundImage: `url(${imageBg.src})`,
+        background: `linear-gradient(rgba(0,0,0,0.8), rgba(0,0,0,0.3)), url(${imageBg.src})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "bottom",
+      }}
+    >
+      <div className="container mx-auto h-100">
+        <div className="row h-100 align-items-center justify-content-end">
+          <div className="col-5">
+            <AccountBox />
+          </div>
         </div>
       </div>
     </div>
